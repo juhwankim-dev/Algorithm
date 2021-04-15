1ccd79dd9da1e93c34a37865ebdbcfb79ea9855e
juhwankim-dev <76620764+juhwankim-dev@users.noreply.github.com>
2021-04-15 22:25:33 +0900

    Create _1_unfinishedRunner.java
    프로그래머스 코딩 테스트 연습
    해시, 완주하지 못한 선수

---
+++ Programmers/practiceCodingTest/_1_unfinishedRunner.java
@@ -0,0 +1,32 @@
+package practiceCodingTest;
+
+import java.util.*;
+
+public class _1_unfinishedRunner {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> hs = new HashMap<String, Integer>();
+        for(String c : completion){
+            hs.put(c, hs.getOrDefault(c, 0) + 1);
+        }
+
+        for(String p : participant){
+            hs.put(p, hs.getOrDefault(p, 0) - 1);
+
+            if(hs.get(p) < 0){
+                answer = p;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+// getOrDefault라는 메서드를 처음 알았다...
